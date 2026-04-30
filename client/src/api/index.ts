d6a99655c42d44a1860dcd426cee3479fd09e66b
ManYu Kuo <miya3927@gmail.com>
2026-04-30 08:18:52 -0700

    Added backend url

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -4,4 +4,4 @@ export const BASE_URL = __DEV__
   ? Platform.OS === 'android'
     ? 'http://10.0.2.2:5000'
     : 'http://localhost:5000'
-  : ''; // Future deployed URL
+  : 'https://swappa-backend.onrender.com';
