44b2b55b7e2f26c7211c513698b1f786f4e1465f
ManYu Kuo <miya3927@gmail.com>
2026-04-30 08:31:01 -0700

    Add .env

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -4,4 +4,4 @@ export const BASE_URL = __DEV__
   ? Platform.OS === 'android'
     ? 'http://10.0.2.2:5000'
     : 'http://localhost:5000'
-  : 'https://swappa-backend.onrender.com';
+  : process.env.EXPO_PUBLIC_API_URL;
